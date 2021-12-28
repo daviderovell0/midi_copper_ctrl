f39452cebfe338174966e594500afbfb89b81b2f
daviderovelli <daviderovelli24@gmail.com>
2021-12-28 16:56:43 +0100

    adding old files before updating my machine

--- encoder/encoder.ino
+++ encoder/encoder.ino
@@ -6,8 +6,8 @@
  
 //digital pin 2 has a pushbutton attached to it. Give it a name:
 int knob1_btn = 23;
-int knob1a = 24;
-int knob1b = 25;
+int knob1A = 24;
+int knob1B = 25;
 int k1s_prev;
 
 void buttonPress() {
@@ -15,30 +15,40 @@ void buttonPress() {
   Serial.println("button pressed");
 }
 
-void knobTurn() {
-  if (digitalRead(knob1a) == digitalRead(knob1b)) {
+void knobTurnA() {
+  if (digitalRead(knob1A) == digitalRead(knob1B)) {
     Serial.println("anticlockwise rotation");
   }
   else {
     Serial.println("clockwise rotation");
   }
-  
+}
+
+void knobTurnB() {
+  if (digitalRead(knob1A) != digitalRead(knob1B)) {
+    Serial.println("anticlockwise rotation");
+  }
+  else {
+    Serial.println("clockwise rotation");
+  }
 }
 
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
-  // make the pushbutton's pin an input:
+  
+  // Initialise pins
   pinMode(knob1_btn, INPUT_PULLUP);
-  pinMode(knob1a, INPUT_PULLUP);
-  pinMode(knob1b, INPUT_PULLUP);
+  pinMode(knob1A, INPUT_PULLUP);
+  pinMode(knob1B, INPUT_PULLUP);
 
   // attach interrupts
   attachInterrupt(digitalPinToInterrupt(knob1_btn), buttonPress, FALLING);
-  attachInterrupt(digitalPinToInterrupt(knob1a), knobTurn, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(knob1A), knobTurnA, FALLING);
+  attachInterrupt(digitalPinToInterrupt(knob1B), knobTurnB, RISING);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  
+  // interrupts on
 }
